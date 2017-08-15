228f6e066b6f2ca19c103e0e3a415c746bd83d64
apktool <apktoolOo@gmail.com>
2017-08-15 15:41:16 +0800

    Exception, throws, throw
    Signed-off-by: apktool <apktoolOo@gmail.com>

--- src/ExceptionDemo.java
+++ src/ExceptionDemo.java
@@ -1,6 +1,6 @@
 /**
  * Description:
- * Exception
+ * Exception, throws, throw
  *
  * @author apktool
  * @version 1.0
@@ -12,17 +12,36 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ExceptionDemo {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
         String s = "2017-08-15";
         SimpleDateFormat sdf = new SimpleDateFormat("yyy-MM-dd HH:mm:ss");
-        try{
+        try {
             Date d = sdf.parse(s);
-            System.out.println(d);;
-        }catch (ParseException e){
+            System.out.println(d);
+        } catch (ParseException e) {
             System.out.println(e.getMessage());
             System.out.println(e.toString());
             System.out.println("----------");
             e.printStackTrace();
         }
+
+        System.out.println("----------");
+
+        try {
+            method();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("----------");
+    }
+
+    public static void method() throws Exception {
+        int a = 10;
+        int b = 0;
+        if (b == 0) {
+            throw new Exception();
+        }
+        System.out.println(a / b);
     }
 }
