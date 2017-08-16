37d6106cae15ad04100517b9aa1d187f96ea4665
apktool <apktoolOo@gmail.com>
2017-08-16 22:10:51 +0800

    File Stream | exception, write, read
    Signed-off-by: apktool <apktoolOo@gmail.com>

--- src/FileStreamDemo.java
+++ src/FileStreamDemo.java
@@ -1,11 +1,13 @@
 /**
  * Description:
- * file exception
+ * file exception | write, read
+ *
  * @author apktool
  * @version 1.0
  * @since 2017/8/16
  */
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -13,7 +15,7 @@ import java.io.IOException;
 public class FileStreamDemo {
     public static void main(String[] argv) throws IOException {
         FileOutputStream fos = null;
-        try{
+        try {
             fos = new FileOutputStream("fos.txt");
             fos.write("Hello World".getBytes());
             fos.write("\n".getBytes());
@@ -24,18 +26,23 @@ public class FileStreamDemo {
             byte[] bys = {97, 98, 99, 100, 110};
             fos.write(bys);
             fos.write(bys, 1, 3);
-        }catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
-        }finally {
-            if(fos!=null){
-                try{
+        } finally {
+            if (fos != null) {
+                try {
                     fos.close();
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
+
+        FileInputStream fis = new FileInputStream("fos.txt");
+        byte[] bys = new byte[5];
+        int len = fis.read(bys);
+        System.out.println(new String(bys, 0, len));
     }
 }
