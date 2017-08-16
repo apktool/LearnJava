a17aafd26e52b0454b1a9eec12154c9c498a39a9
apktool <apktoolOo@gmail.com>
2017-08-16 21:10:12 +0800

    File Stream
    Signed-off-by: apktool <apktoolOo@gmail.com>

---
+++ src/FileStreamDemo.java
@@ -0,0 +1,41 @@
+/**
+ * Description:
+ * file exception
+ * @author apktool
+ * @version 1.0
+ * @since 2017/8/16
+ */
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileStreamDemo {
+    public static void main(String[] argv) throws IOException {
+        FileOutputStream fos = null;
+        try{
+            fos = new FileOutputStream("fos.txt");
+            fos.write("Hello World".getBytes());
+            fos.write("\n".getBytes());
+
+            fos.write(97);
+            fos.write("\n".getBytes());
+
+            byte[] bys = {97, 98, 99, 100, 110};
+            fos.write(bys);
+            fos.write(bys, 1, 3);
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if(fos!=null){
+                try{
+                    fos.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
