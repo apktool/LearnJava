44b64656ee56ff83dfe85c4f024125056ae327bb
apktool <apktoolOo@gmail.com>
2017-11-02 16:10:01 +0800

    Spring boot demo | interceptor demo

---
+++ SpringbootDemo/src/main/java/com/example/demo/conf/interceptor/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.example.demo.conf.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addInterceptors(InterceptorRegistry interceptorRegistry) {
+        interceptorRegistry.addInterceptor(new LoggingInterceptor()).addPathPatterns("/*");
+    }
+}
+
