c50e4feef1b28e02ecd39857e1cd0c4504900ae5
apktool <apktoolOo@gmail.com>
2017-11-27 20:46:57 +0800

    Spring Injection | Injection by setting function

--- SpringDemo/src/main/java/com/custome/CustomServiceImpl.java
+++ SpringDemo/src/main/java/com/custome/CustomServiceImpl.java
@@ -3,7 +3,13 @@ package com.custome;
 public class CustomServiceImpl implements CustomServiceInterface{
     private CustomDaoImpl customDao;
 
+    /*
     public CustomServiceImpl(CustomDaoImpl customDao){
+        this.customDao = customDao;
+    }
+    */
+
+    public void setCustomDao(CustomDaoImpl customDao){
         this.customDao = customDao;
     }
 
