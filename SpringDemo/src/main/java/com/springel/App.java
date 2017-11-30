7b1ed91fb81fce358839970b6d00061cd71d0bd5
apktool <apktoolOo@gmail.com>
2017-11-30 21:33:41 +0800

    SpringEL | Operate Map and List

--- SpringDemo/src/main/java/com/springel/App.java
+++ SpringDemo/src/main/java/com/springel/App.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
-        CustomDaoImpl customerDao = (CustomDaoImpl) context.getBean("customerDao");
-        System.out.println(customerDao);
+        Customer customer = (Customer) context.getBean("customer");
+        System.out.println(customer.getName() + " : " + customer.getTelephone());
     }
 }
