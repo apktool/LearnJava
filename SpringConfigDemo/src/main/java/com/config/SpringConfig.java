d2d468956cf80de7ed7dc19c1a91b1a48efad21f
apktool <apktoolOo@gmail.com>
2017-12-01 14:33:28 +0800

    Spring ZeroXML | Basic

---
+++ SpringConfigDemo/src/main/java/com/config/SpringConfig.java
@@ -0,0 +1,10 @@
+package com.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+    public SpringConfig() {
+        System.out.println("Spring environments is initialized successfully.");
+    }
+}
