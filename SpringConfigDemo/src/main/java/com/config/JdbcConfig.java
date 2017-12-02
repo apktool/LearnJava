2267b29e585249803d5a794e13aa11b19381dee7
apktool <apktoolOo@gmail.com>
2017-12-02 10:35:32 +0800

    Spring ZeroXML | @Import

---
+++ SpringConfigDemo/src/main/java/com/config/JdbcConfig.java
@@ -0,0 +1,7 @@
+package com.config;
+
+import org.springframework.context.annotation.PropertySource;
+
+@PropertySource(value = "classpath:jdbc.properties")
+public class JdbcConfig {
+}
--- SpringConfigDemo/src/main/java/com/config/SpringConfig.java
+++ SpringConfigDemo/src/main/java/com/config/SpringConfig.java
@@ -2,11 +2,12 @@ package com.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = {"com.config"})
-@PropertySource(value = "classpath:jdbc.properties")
+@Import(JdbcConfig.class)
 public class SpringConfig {
     public SpringConfig() {
         System.out.println("Spring environments is initialized successfully.");
