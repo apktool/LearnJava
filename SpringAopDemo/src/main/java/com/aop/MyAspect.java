8126e04e33a67fe0447495af86a0af97115eea11
apktool <apktoolOo@gmail.com>
2017-12-04 15:04:04 +0800

    Spring AOP | Before advice

--- SpringAopDemo/src/main/java/com/aop/MyAspect.java
+++ SpringAopDemo/src/main/java/com/aop/MyAspect.java
@@ -1,7 +1,18 @@
 package com.aop;
 
+import org.aspectj.lang.JoinPoint;
+
+import java.util.Arrays;
+
 public class MyAspect {
     public void WriteLog() {
         System.out.println("The log is write by Spring AOP");
+    }
+
+    public void before(JoinPoint jp) {
+        System.out.println("Before advice");
+        System.out.println("Proxy object type: " + jp.getThis().getClass());
+        System.out.println("The function intercepted: " + jp.getSignature().getName());
+        System.out.println("The args intercepted" + Arrays.asList(jp.getArgs()));
     }
 }
