a72879602c7f446fa2e187fd028c87fbbd2486ca
apktool <apktoolOo@gmail.com>
2017-12-16 20:36:15 +0800

    Hibernate | Anotation

--- HibernateDemo/src/main/java/com/hibernate/Employee.java
+++ HibernateDemo/src/main/java/com/hibernate/Employee.java
@@ -4,13 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "EMPLOYEE")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "salary")
     private int salary;
 
     public Employee(String fname, String lname, int salary) {
