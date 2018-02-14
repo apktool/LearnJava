768eb95c4866fa9cf55a644ce8ca36a5856fb0b0
apktool <apktoolOo@gmail.com>
2018-02-14 11:22:27 +0800

    JVM | 创建线程导致内存溢出异常

---
+++ JVM/src/main/java/com/chapter2/codelist5/JavaVMStackOOM.java
@@ -0,0 +1,32 @@
+/**
+ * 创建线程导致内存溢出异常
+ *
+ * JVM args:
+ * -Xss256k
+ */
+
+package com.chapter2.codelist5;
+
+public class JavaVMStackOOM {
+    private void dontStop() {
+        while (true) {
+        }
+    }
+
+    public void stackLeakByThread() {
+        while (true) {
+            Thread thread = new Thread(new Runnable() {
+                public void run() {
+                    dontStop();
+                }
+            });
+
+            thread.start();
+        }
+    }
+
+    public static void main(String[] args) {
+        JavaVMStackOOM oom = new JavaVMStackOOM();
+        oom.stackLeakByThread();
+    }
+}
