7ea27b6850e8347aee9dccc5db1e1d964e4ddb10
apktool <apktoolOo@gmail.com>
2018-02-19 18:57:17 +0800

    JVM | 静态解析

---
+++ JVM/src/main/java/com/chapter8/codelist5/StaticResolution.java
@@ -0,0 +1,11 @@
+package com.chapter8.codelist5;
+
+public class StaticResolution {
+    public static void sayHello() {
+        System.out.println("Hello world");
+    }
+
+    public static void main(String[] args) {
+        StaticResolution.sayHello();
+    }
+}
